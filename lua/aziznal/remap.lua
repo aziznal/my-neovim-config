8efc5c8eb9003021955115a745d633e41b4b1cb1
Aziz <aziznal.dev@gmail.com>
2023-05-11 15:27:06 +0300

    setup config including telescope, github theme, treesitter, harpoon, and undo tree

--- lua/aziznal/remap.lua
+++ lua/aziznal/remap.lua
@@ -3,7 +3,3 @@ vim.g.mapleader = " "
 
 -- map explorer command to make it easier to open
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
--- center screen when jumping half a page
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
