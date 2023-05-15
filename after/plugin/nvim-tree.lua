b71803827e2512f801d53a9ce0f87bca70600ec7
Aziz <aziznal.dev@gmail.com>
2023-05-15 22:54:41 +0300

    disable nvim-tree

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,7 +1,9 @@
-require("nvim-tree").setup {
-    sort_by = "case_sensitive",
+-- require("nvim-tree").setup {
+--    sort_by = "case_sensitive",
 
-    view = {
-        width = 50,
-    },
-}
+--    view = {
+--        width = 50,
+--    },
+-- }
+--
+-- vim.keymap.set("n", "<leader><C-e>", ":NvimTreeFindFileToggle<CR>", { noremap = true, silent = true })
