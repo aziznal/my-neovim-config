52f9ef74eba0de850b889080e7cd4657bf178665
Abdulaziz Nal <aziznal.dev@gmail.com>
2023-05-31 09:08:45 +0300

    Re-install harpoon and assign C-l for copilot accept

--- lua/aziznal/packer.lua
+++ lua/aziznal/packer.lua
@@ -7,18 +7,18 @@ return require('packer').startup(function(use)
 
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
     }
+    
+    -- required for telescope
+    use ('nvim-lua/plenary.nvim')
 
     use({
         'projekt0n/github-nvim-theme', tag = 'v0.0.7',
-        -- or                            branch = '0.0.x'
     })
 
     use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
-    use ('theprimeagen/harpoon')
+    use ('ThePrimeagen/harpoon')
 
     use ('mbbill/undotree')
 
