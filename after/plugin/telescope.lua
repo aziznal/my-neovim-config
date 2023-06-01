6752a0eab09c782d34b4078c496cf85317b1ef37
aziznal <aziznal.dev@gmail.com>
2023-06-01 09:32:50 +0300

    update telescope settings to hide file preview

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,12 +1,20 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+
+local function find_files()
+	builtin.find_files { previewer= false }
+end
+
+local function find_git_files()
+	builtin.git_files { previewer= false }
+end
+
+vim.keymap.set('n', '<leader>pf', find_files , {})
+vim.keymap.set('n', '<C-p>', find_git_files , {})
 
 -- fuzzy file finding
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
-
 
 -- recent projects (integration from project explorer plugin)
 require('telescope').load_extension('projects')
