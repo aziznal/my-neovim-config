92d857972cc620e710ed781da5c6663fb2e2acec
aziznal <aziznal.dev@gmail.com>
2023-06-03 00:30:39 +0300

    add lualine plugin

---
+++ after/plugin/lualine.lua
@@ -0,0 +1,22 @@
+require('lualine').setup({
+    options = {
+        theme = require('lualine.themes.material')
+    },
+
+    icons_enabled = true,
+
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'searchcount', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+    },
+
+    tabline = {
+
+    },
+
+    globalstatus = false,
+})
