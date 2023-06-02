542d399af4e67f88fb3ab8cb729c4f36fb8539fe
aziznal <aziznal.dev@gmail.com>
2023-06-03 01:46:18 +0300

    Re-enable nvim-tree plugin

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,9 +1,30 @@
--- require("nvim-tree").setup {
---    sort_by = "case_sensitive",
+-- disable netrw to avoid conflicts
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
---    view = {
---        width = 50,
---    },
--- }
+-- Main Shortcuts:
+-- <leader><C-e> - toggle tree with current file selected 
+-- a - create a file or directory
+-- d - remove a file or directory
+-- r - rename a file or directory
+
+-- <CR> - expand dir / open file
+-- W - collapse
+-- E - expand recursively
 --
--- vim.keymap.set("n", "<leader><C-e>", ":NvimTreeFindFileToggle<CR>", { noremap = true, silent = true })
+-- <C-v> - split
+-- <C-x> - vsplit
+
+require("nvim-tree").setup {
+    sort_by = "case_sensitive",
+
+    view = {
+        width = 40,
+    },
+
+    filters = {
+        dotfiles = false,
+    }
+}
+
+vim.keymap.set("n", "<leader><C-e>", ":NvimTreeFindFileToggle<CR>", { noremap = true, silent = true })
