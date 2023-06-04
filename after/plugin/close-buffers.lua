3e4dde1e85052ab106ca5cba8dedd6099affdc2b
Aziz <aziznal.dev@gmail.com>
2023-06-04 14:50:39 +0300

    add shortcut for closing current buffer

--- after/plugin/close-buffers.lua
+++ after/plugin/close-buffers.lua
@@ -3,3 +3,9 @@ require('close_buffers').setup({ })
 -- Commands:
 -- :Bdelete this | other | all | hidden | modified
 -- add bang (!) to force delete
+
+
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+map('n', '<leader>x', ':BDelete this<CR>', opts)
