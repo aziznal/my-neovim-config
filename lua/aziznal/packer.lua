8815a128afdf28f6b23adf13fd61374cc6bcdbb2
Aziz <aziznal.dev@gmail.com>
2023-06-08 20:27:09 +0300

    Install autotag plugin for closing html tags

--- lua/aziznal/packer.lua
+++ lua/aziznal/packer.lua
@@ -18,6 +18,8 @@ return require('packer').startup(function(use)
 
     use ('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
 
+    use ('windwp/nvim-ts-autotag')
+
     use ('mbbill/undotree')
 
     -- LSP Zero
