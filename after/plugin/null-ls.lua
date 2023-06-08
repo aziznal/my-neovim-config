1565bc3f3e65aafc5a2194e4b87890d76385b9ab
Aziz <aziznal.dev@gmail.com>
2023-06-08 20:14:30 +0300

    Disable autoformat on save

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -7,26 +7,37 @@ local async = event == "BufWritePost"
 null_ls.setup({
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
-      vim.keymap.set("n", "<Leader>f", function()
+      vim.keymap.set("n", "<Leader>f",
+
+      function()
         vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-      end, { buffer = bufnr, desc = "[lsp] format" })
+      end,
+
+      { buffer = bufnr, desc = "[lsp] format" })
 
       -- format on save
-      vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+      --[[ vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+
       vim.api.nvim_create_autocmd(event, {
         buffer = bufnr,
         group = group,
+
         callback = function()
           vim.lsp.buf.format({ bufnr = bufnr, async = async })
         end,
+
         desc = "[lsp] format on save",
-      })
+      }) ]]
     end
 
     if client.supports_method("textDocument/rangeFormatting") then
-      vim.keymap.set("x", "<Leader>f", function()
+      vim.keymap.set("x", "<Leader>f",
+
+      function()
         vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-      end, { buffer = bufnr, desc = "[lsp] format" })
+      end,
+
+      { buffer = bufnr, desc = "[lsp] format" })
     end
   end,
 })
