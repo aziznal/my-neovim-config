9c7bed347a85e6da13fdcab5a6f850f81f5b196f
Aziz <aziznal.dev@gmail.com>
2023-07-11 21:12:50 +0300

    add shortcut for closing all buffers

--- after/plugin/close-buffers.lua
+++ after/plugin/close-buffers.lua
@@ -9,3 +9,4 @@ local opts = {noremap = true, silent = true}
 
 map("n", "<leader>x", ":BDelete this<CR>", opts)
 map("n", "<leader>X", ":BDelete other<CR>", opts)
+map("n", "<leader>Z", ":BDelete all<CR>", opts)
