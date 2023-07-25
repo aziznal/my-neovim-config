6f6bc06544e2c100c4d214d2504cfa113e4f9fa2
Aziz <aziznal.dev@gmail.com>
2023-07-25 22:32:59 +0300

    add lightbulb plugin for indication code action availability

---
+++ after/plugin/lightbulb.lua
@@ -0,0 +1,9 @@
+require("nvim-lightbulb").setup(
+    {
+        autocmd = {enabled = true, updateTime = 50},
+        priority = 100,
+        sign = {
+            enabled = true
+        }
+    }
+)
