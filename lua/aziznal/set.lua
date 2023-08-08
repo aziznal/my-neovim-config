6c6691d32e39c61e0bb83ea88a097c48635f6070
aziznal <aziznal.dev@gmail.com>
2023-08-08 23:54:55 +0300

    enable incremental search

--- lua/aziznal/set.lua
+++ lua/aziznal/set.lua
@@ -16,8 +16,8 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = false
+-- vim.opt.hlsearch = false
+-- vim.opt.incsearch = false
 
 vim.opt.termguicolors = true
 
