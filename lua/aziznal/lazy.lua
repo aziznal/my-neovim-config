bd8175e4f0c345605bc82a888c2855148412016f
aziznal <aziznal.dev@gmail.com>
2023-08-25 23:57:08 +0300

    configure lazy to disable auto-updates and config change detection

--- lua/aziznal/lazy.lua
+++ lua/aziznal/lazy.lua
@@ -1,16 +1,30 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system(
+        {
+            "git",
+            "clone",
+            "--filter=blob:none",
+            "https://github.com/folke/lazy.nvim.git",
+            "--branch=stable", -- latest stable release
+            lazypath
+        }
+    )
 end
 
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("aziznal.plugins")
+require("lazy").setup(
+    {
+        {import = "aziznal.plugins"}
+    },
+    {
+        checker = {
+            enabled = false
+        },
+        change_detection = {
+            notify = false
+        }
+    }
+)
