9d9f0110826b0e6f52db9c51f383749bea84c8ea
aziznal <aziznal.dev@gmail.com>
2023-08-30 20:16:28 +0300

    remove rust from treesitter ensure_installed

--- lua/aziznal/plugins/treesitter.lua
+++ lua/aziznal/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         require "nvim-treesitter.configs".setup {
             -- A list of parser names, or "all" (the five listed parsers should always be installed)
-            ensure_installed = {"javascript", "typescript", "rust", "lua", "vim", "vimdoc", "query"},
+            ensure_installed = {"javascript", "typescript", "lua", "vim", "vimdoc", "query"},
             -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
             -- Automatically install missing parsers when entering buffer
