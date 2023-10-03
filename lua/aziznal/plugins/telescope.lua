00816592141a86e9bdd7de0280f4bbf056921049
Aziz <aziznal.dev@gmail.com>
2023-10-04 00:33:00 +0300

    replace nvim-tree with telescope-file-browser

--- lua/aziznal/plugins/telescope.lua
+++ lua/aziznal/plugins/telescope.lua
@@ -54,5 +54,33 @@ return {
                 builtin.live_grep()
             end
         )
+
+        -- file browser extension
+        require("telescope").setup {
+            extensions = {
+                file_browser = {
+                    initial_mode = "normal",
+                    sorting_strategy = "ascending",
+                    -- disables netrw and use telescope-file-browser in its place
+                    hijack_netrw = true,
+                    mappings = {
+                        ["i"] = {},
+                        ["n"] = {}
+                    }
+                }
+            }
+        }
+
+        require("telescope").load_extension "file_browser"
+
+        vim.api.nvim_set_keymap("n", "<space>fb", ":Telescope file_browser<CR>", {noremap = true})
+
+        -- open file_browser with the path of the current buffer
+        vim.api.nvim_set_keymap(
+            "n",
+            "<leader><C-e>",
+            ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+            {noremap = true}
+        )
     end
 }
