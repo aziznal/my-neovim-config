6b61cc3822b048a124f11e923fc398bda4a115c8
Aziz <aziznal.dev@gmail.com>
2023-10-15 21:07:55 +0300

    add better commenting in react and advanced selection shortcuts using treesitter
    - Big thanks to https://github.com/josean-dev/dev-environment-files.

--- lua/aziznal/plugins/comment.lua
+++ lua/aziznal/plugins/comment.lua
@@ -3,7 +3,9 @@ return {
     "numToStr/Comment.nvim",
     event = "VeryLazy",
     config = function()
-        require("Comment").setup()
+        require("Comment").setup {
+            pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
+        }
 
         -- Basic Mappings
         -- `gcc` - Toggles the current line using linewise comment
