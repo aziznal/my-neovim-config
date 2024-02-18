aed66386102c0acbdf1a0d6e30a374b47534d7aa
aziznal <aziznal.dev@gmail.com>
2024-02-18 23:00:51 +0300

    install boole.nvim

---
+++ lua/aziznal/plugins/boole.lua
@@ -0,0 +1,19 @@
+return {
+    "nat-418/boole.nvim",
+    config = function()
+        require("boole").setup {
+            mappings = {
+                increment = "<C-a>",
+                decrement = "<C-x>"
+            },
+            -- User defined loops
+            additions = {
+                {"Foo", "Bar"},
+                {"tic", "tac", "toe"}
+            },
+            allow_caps_additions = {
+                {"enable", "disable"}
+            }
+        }
+    end
+}
