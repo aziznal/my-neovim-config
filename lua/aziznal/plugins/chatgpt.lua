85259a9c4f71f620f3f5537881bb199c1cd50e7c
aziznal <aziznal.dev@gmail.com>
2024-02-24 15:52:51 +0300

    increase chatgpt max tokens

--- lua/aziznal/plugins/chatgpt.lua
+++ lua/aziznal/plugins/chatgpt.lua
@@ -152,7 +152,7 @@ return {
                 model = "gpt-4",
                 frequency_penalty = 0,
                 presence_penalty = 0,
-                max_tokens = 300,
+                max_tokens = 3000,
                 temperature = 0,
                 top_p = 1,
                 n = 1
