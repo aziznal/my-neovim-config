180813eebfc4275a89c24c4dbb1a6c399899d770
aziznal <aziznal.dev@gmail.com>
2024-03-06 01:10:57 +0300

    add indent=true property to treesitter for some reason

--- lua/aziznal/plugins/nvim-treesitter.lua
+++ lua/aziznal/plugins/nvim-treesitter.lua
@@ -21,6 +21,9 @@ return {
 				-- Instead of true it can also be a list of languages
 				additional_vim_regex_highlighting = false,
 			},
+			indent = {
+				enable = true,
+			},
 			incremental_selection = {
 				enable = true,
 				keymaps = {
