1fe780652b9f100f330b1071ba70f25bf6826240
aziznal <aziznal.dev@gmail.com>
2024-03-05 22:26:36 +0300

    update autocmd group name

--- lua/aziznal/autocmds.lua
+++ lua/aziznal/autocmds.lua
@@ -1,7 +1,10 @@
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
+vim.api.nvim_create_autocmd(
+    "TextYankPost",
+    {
+        desc = "Highlight when yanking (copying) text",
+        group = vim.api.nvim_create_augroup("highlight-yank", {clear = true}),
+        callback = function()
+            vim.highlight.on_yank()
+        end
+    }
+)
