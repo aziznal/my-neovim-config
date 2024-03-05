77e5ad2a78f97d97854400475e5c8f40e8b556ba
aziznal <aziznal.dev@gmail.com>
2024-03-05 21:48:58 +0300

    add vim-sleuth

--- lua/aziznal/plugins/guess-indent.lua
+++ lua/aziznal/plugins/guess-indent.lua
@@ -1,12 +1,12 @@
 -- auto detect indentation
 return {
-    "nmac427/guess-indent.nvim",
-    event = "BufRead",
-    config = function()
-        require("guess-indent").setup {
-            -- run command when a new buffer is opened
-            auto_cmd = true,
-            override_editorconfig = true
-        }
-    end
+    -- "nmac427/guess-indent.nvim",
+    -- event = "BufRead",
+    -- config = function()
+    --     require("guess-indent").setup {
+    --         -- run command when a new buffer is opened
+    --         auto_cmd = true,
+    --         override_editorconfig = true
+    --     }
+    -- end
 }
