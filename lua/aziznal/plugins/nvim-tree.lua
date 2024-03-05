053e1c71207814fb3e777948def0cc6adcba04ad
aziznal <aziznal.dev@gmail.com>
2024-03-06 00:32:21 +0300

    update nvim-treesitter ts-context commentstring plugin

--- lua/aziznal/plugins/nvim-tree.lua
+++ lua/aziznal/plugins/nvim-tree.lua
@@ -1,59 +1,57 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = {"nvim-tree/nvim-web-devicons"},
-    config = function()
-        -- disable netrw to avoid conflicts
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
+	"nvim-tree/nvim-tree.lua",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		-- disable netrw to avoid conflicts
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
 
-        -- Main Shortcuts:
-        -- <leader><C-e> - toggle tree with current file selected
-        -- a - create a file or directory
-        -- d - remove a file or directory
-        -- r - rename a file or directory
+		-- Main Shortcuts:
+		-- <leader><C-e> - toggle tree with current file selected
+		-- a - create a file or directory
+		-- d - remove a file or directory
+		-- r - rename a file or directory
 
-        -- <CR> - expand dir / open file
-        -- W - collapse
-        -- E - expand recursively
-        --
-        -- <C-v> - split
-        -- <C-x> - vsplit
+		-- <CR> - expand dir / open file
+		-- W - collapse
+		-- E - expand recursively
+		--
+		-- <C-v> - split
+		-- <C-x> - vsplitnvim-tree
 
-        require("nvim-tree").setup {
-            sort_by = "case_sensitive",
-            view = {
-                cursorline = true,
-                side = "left",
-                width = 45
-            },
-            filters = {
-                dotfiles = false
-            },
-            renderer = {
-                highlight_opened_files = "name",
-                highlight_modified = "name",
-                full_name = true
-            },
-            git = {
-                enable = true,
-                ignore = false
-            },
-            update_focused_file = {
-                enable = true
-            },
-            diagnostics = {
-                enable = true
-            }
-        }
+		require("nvim-tree").setup({
+			sort_by = "case_sensitive",
+			view = {
+				cursorline = true,
+				side = "left",
+				width = {
+					padding = 0,
+					min = 45,
+				},
+			},
+			filters = {
+				dotfiles = false,
+			},
+			renderer = {
+				highlight_opened_files = "name",
+				highlight_modified = "name",
+				full_name = true,
+			},
+			git = {
+				enable = true,
+				ignore = false,
+			},
+			update_focused_file = {
+				enable = true,
+			},
+			diagnostics = {
+				enable = true,
+			},
+		})
 
-        vim.keymap.set(
-            "n",
-            "<leader><C-n>",
-            ":NvimTreeFindFileToggle<CR>",
-            {
-                noremap = true,
-                silent = true
-            }
-        )
-    end
+		vim.keymap.set("n", "<leader><C-n>", ":NvimTreeFindFileToggle<CR>", {
+			noremap = true,
+			silent = true,
+		})
+	end,
 }
