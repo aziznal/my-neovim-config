674819f3cdf64e4f257983c9ff254ef8eec19312
aziznal <aziznal.dev@gmail.com>
2024-03-06 12:03:24 +0300

    remove ref highlight on text hover

--- lua/aziznal/plugins/nvim-lspconfig.lua
+++ lua/aziznal/plugins/nvim-lspconfig.lua
@@ -73,29 +73,6 @@ return {
                         "Restart LspServer"
                     )
 
-                    -- The following two autocommands are used to highlight references of the
-                    -- word under your cursor when your cursor rests there for a little while.
-                    --    See `:help CursorHold` for information about when this is executed
-                    --
-                    -- When you move your cursor, the highlights will be cleared (the second autocommand).
-                    local client = vim.lsp.get_client_by_id(event.data.client_id)
-                    if client and client.server_capabilities.documentHighlightProvider then
-                        vim.api.nvim_create_autocmd(
-                            {"CursorHold", "CursorHoldI"},
-                            {
-                                buffer = event.buf,
-                                callback = vim.lsp.buf.document_highlight
-                            }
-                        )
-
-                        vim.api.nvim_create_autocmd(
-                            {"CursorMoved", "CursorMovedI"},
-                            {
-                                buffer = event.buf,
-                                callback = vim.lsp.buf.clear_references
-                            }
-                        )
-                    end
                 end
             }
         )
