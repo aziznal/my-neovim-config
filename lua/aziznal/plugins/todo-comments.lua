f57242569a5a3e12c73370658e3e633a057534f3
aziznal <aziznal.dev@gmail.com>
2024-03-06 21:04:47 +0300

    update todo-comment styles

--- lua/aziznal/plugins/todo-comments.lua
+++ lua/aziznal/plugins/todo-comments.lua
@@ -67,7 +67,7 @@ return {
 				multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
 				multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
 				before = "", -- "fg" or "bg" or empty
-				keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+				keyword = "bg", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
 				after = "fg", -- "fg" or "bg" or empty
 				pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
 				comments_only = true, -- uses treesitter to match keywords in comments only
