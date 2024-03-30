b26129980a39a762a1dd7ff1d2e1c7ba2a7532c1
aziznal <aziznal.dev@gmail.com>
2024-03-30 22:53:32 +0300

    disable copilot by default

--- lua/aziznal/plugins/copilot.lua
+++ lua/aziznal/plugins/copilot.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		require("copilot").setup({
 			suggestion = {
-				enabled = true,
+				enabled = false,
 				auto_trigger = true,
 				keymap = {
 					accept = "<C-a>",
