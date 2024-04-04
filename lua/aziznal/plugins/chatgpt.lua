47c89c585ac9de12d4a564032bf5baba17eb371a
aziznal <aziznal.dev@gmail.com>
2024-04-04 23:34:01 +0300

    update chatgpt message bandwidth

--- lua/aziznal/plugins/chatgpt.lua
+++ lua/aziznal/plugins/chatgpt.lua
@@ -152,7 +152,8 @@ return {
 				model = "gpt-4",
 				frequency_penalty = 0,
 				presence_penalty = 0,
-				max_tokens = 7000,
+				-- actual max tokens is 8192. below count is just for response and the bandwidth for input and response is SHARED!
+				max_tokens = 4000,
 				temperature = 0,
 				top_p = 1,
 				n = 1,
