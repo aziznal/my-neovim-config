8ad706731b1c833ac076115bf6bdbf27162aa908
aziznal <aziznal.dev@gmail.com>
2024-06-30 16:46:50 +0300

    update all plugins (and customize transparent plugin to maintain consistent behavior)

--- lua/aziznal/plugins/transparent.lua
+++ lua/aziznal/plugins/transparent.lua
@@ -1,6 +1,40 @@
 return {
 	"xiyaowong/transparent.nvim",
 	config = function()
-		require("transparent").setup({})
+		require("transparent").setup({ -- Optional, you don't have to run setup.
+			groups = { -- table: default groups
+				"Normal",
+				"NormalNC",
+				"Comment",
+				"Constant",
+				"Special",
+				"Identifier",
+				"Statement",
+				"PreProc",
+				"Type",
+				"Underlined",
+				"Todo",
+				"String",
+				"Function",
+				"Conditional",
+				"Repeat",
+				"Operator",
+				"Structure",
+				"LineNr",
+				"NonText",
+				"SignColumn",
+				"CursorLineNr",
+				"StatusLine",
+				"StatusLineNC",
+				"EndOfBuffer",
+			},
+			extra_groups = {
+				"NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
+				-- "NvimTreeNormal", -- NvimTree
+			}, -- table: additional groups that should be cleared
+			exclude_groups = {
+				"CursorLine",
+			}, -- table: groups you don't want to clear
+		})
 	end,
 }
