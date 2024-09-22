92c9a02cd5f4278d139b206e4968f72904dc017b
aziznal <aziznal.dev@gmail.com>
2024-09-22 15:42:42 +0300

    make Enter accept autocomplete suggestions

--- lua/aziznal/plugins/nvim-cmp.lua
+++ lua/aziznal/plugins/nvim-cmp.lua
@@ -272,6 +272,7 @@ return {
 				--  This will auto-import if your LSP supports it.
 				--  This will expand snippets if the LSP sent a snippet.
 				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
 				-- Manually trigger a completion from nvim-cmp.
 				--  Generally you don't need this, because nvim-cmp will display
 				--  completions whenever it has completion options available.
