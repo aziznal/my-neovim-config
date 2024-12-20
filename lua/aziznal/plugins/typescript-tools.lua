ac77c074abf6d9ca57b58779be93d555c3fd9329
aziznal <aziznal.dev@gmail.com>
2024-12-20 10:55:25 +0300

    add custom commands for removing unused imports in typescript

--- lua/aziznal/plugins/typescript-tools.lua
+++ lua/aziznal/plugins/typescript-tools.lua
@@ -3,5 +3,13 @@ return {
 	requires = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
 	config = function()
 		require("typescript-tools").setup({})
+
+		-- sometimes I'm too lazy to type the TSTools prefix
+		vim.api.nvim_create_user_command("RemoveUnusedImports", function()
+			vim.cmd("TSToolsRemoveUnusedImports")
+		end, {})
+
+		-- Sometimes I'm too lazy to remember which custom commands I created
+		vim.cmd(":cnoreabbrev rmu TSToolsRemoveUnusedImports")
 	end,
 }
