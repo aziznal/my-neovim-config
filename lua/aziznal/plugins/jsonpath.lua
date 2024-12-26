25b830b7342a2cbc3f2f4bd2465b6af73f71b8f2
aziznal <aziznal.dev@gmail.com>
2024-12-26 12:54:20 +0300

    add command to copy json path and use it in livegrep

--- lua/aziznal/plugins/jsonpath.lua
+++ lua/aziznal/plugins/jsonpath.lua
@@ -1,10 +1,23 @@
 return {
 	"phelipetls/jsonpath.nvim",
 	config = function()
+		local jsonpath = require("jsonpath")
+
 		vim.api.nvim_create_user_command("CopyJsonPath", function()
 			pcall(function()
-				vim.fn.setreg("+", require("jsonpath").get())
+				vim.fn.setreg("+", jsonpath.get())
 			end)
-		end, {})
+		end, { desc = "Copies the path to json property under the cursor" })
+
+		vim.api.nvim_create_user_command("FindTranslation", function()
+			pcall(function()
+				local live_grep = require("telescope.builtin").live_grep
+
+				vim.cmd("CopyJsonPath")
+				live_grep({
+					default_text = jsonpath.get(),
+				})
+			end)
+		end, { desc = "Uses CopyJsonPath then runs toggles a ripgrep search result in telescope using it" })
 	end,
 }
