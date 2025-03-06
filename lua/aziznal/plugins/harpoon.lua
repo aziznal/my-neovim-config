19333c620fc7505fc6e1bf18b2cd643ae90cb3d4
aziznal <aziznal.dev@gmail.com>
2025-03-06 13:36:27 +0300

    replace deprecated harpoon call

--- lua/aziznal/plugins/harpoon.lua
+++ lua/aziznal/plugins/harpoon.lua
@@ -8,7 +8,7 @@ return {
 		harpoon.setup()
 
 		vim.keymap.set("n", "<leader>a", function()
-			harpoon:list():append()
+			harpoon:list():add()
 		end)
 
 		vim.keymap.set("n", "<C-Tab>", function()
