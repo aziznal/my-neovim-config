07d2f011f9b0436d4e0e6b9590a44ff79e49e527
aziznal <aziznal.dev@gmail.com>
2025-03-14 13:31:59 +0300

    add command abbreviation for adding missing imports in typescript

--- lua/aziznal/plugins/typescript-tools.lua
+++ lua/aziznal/plugins/typescript-tools.lua
@@ -4,12 +4,7 @@ return {
 	config = function()
 		require("typescript-tools").setup({})
 
-		-- sometimes I'm too lazy to type the TSTools prefix
-		vim.api.nvim_create_user_command("RemoveUnusedImports", function()
-			vim.cmd("TSToolsRemoveUnusedImports")
-		end, {})
-
-		-- Sometimes I'm too lazy to remember which custom commands I created
 		vim.cmd(":cnoreabbrev rmu TSToolsRemoveUnusedImports")
+		vim.cmd(":cnoreabbrev amu TSToolsAddMissingImports")
 	end,
 }
