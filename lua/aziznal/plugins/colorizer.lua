c08d2e05feb9716c0fc94fb56defef0ed32d07a4
aziznal <aziznal.dev@gmail.com>
2025-05-03 20:42:41 +0300

    disable name colorizing

--- lua/aziznal/plugins/colorizer.lua
+++ lua/aziznal/plugins/colorizer.lua
@@ -6,7 +6,7 @@ return {
 			user_default_options = {
 				RGB = true, -- #RGB hex codes
 				RRGGBB = true, -- #RRGGBB hex codes
-				names = true, -- "Name" codes like Blue or blue
+				names = false, -- "Name" codes like Blue or blue
 				RRGGBBAA = true, -- #RRGGBBAA hex codes
 				AARRGGBB = true, -- 0xAARRGGBB hex codes
 				rgb_fn = true, -- CSS rgb() and rgba() functions
