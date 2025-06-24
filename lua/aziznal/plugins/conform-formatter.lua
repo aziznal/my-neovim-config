e20608c3ac9e04dfcc31fa19196bdbf00603e0a4
aziznal <aziznal.dev@gmail.com>
2025-06-24 14:39:46 +0300

    add pg_format for formatting SQL

--- lua/aziznal/plugins/conform-formatter.lua
+++ lua/aziznal/plugins/conform-formatter.lua
@@ -1,34 +1,36 @@
 -- https://github.com/stevearc/conform.nvim
 return {
-	"stevearc/conform.nvim",
-	config = function()
-		require("conform").setup({
-			notify_on_error = false,
+  "stevearc/conform.nvim",
+  config = function()
+    require("conform").setup({
+      notify_on_error = false,
 
-			formatters_by_ft = {
-				lua = { "stylua", stop_after_first = true },
-				css = { "prettierd", stop_after_first = true },
-				html = { "prettierd", stop_after_first = true },
+      formatters_by_ft = {
+        lua = { "stylua", stop_after_first = true },
+        css = { "prettierd", stop_after_first = true },
+        html = { "prettierd", stop_after_first = true },
 
-				javascript = { "prettierd", stop_after_first = true, timeout_ms = 2000 },
-				javascriptreact = { "prettierd", stop_after_first = true, timeout_ms = 2000 },
-				typescript = { "prettierd", stop_after_first = true, timeout_ms = 2000 },
-				typescriptreact = { "prettierd", stop_after_first = true, timeout_ms = 2000 },
+        javascript = { "prettierd", stop_after_first = true, timeout_ms = 2000 },
+        javascriptreact = { "prettierd", stop_after_first = true, timeout_ms = 2000 },
+        typescript = { "prettierd", stop_after_first = true, timeout_ms = 2000 },
+        typescriptreact = { "prettierd", stop_after_first = true, timeout_ms = 2000 },
 
-				markdown = { "prettierd", stop_after_first = true },
-				json = { "prettierd", stop_after_first = true },
-				jsonc = { "prettierd", stop_after_first = true },
-				go = { "gofmt", stop_after_first = true },
+        markdown = { "prettierd", stop_after_first = true },
+        json = { "prettierd", stop_after_first = true },
+        jsonc = { "prettierd", stop_after_first = true },
+        go = { "gofmt", stop_after_first = true },
 
-				rust = { "rustfmt", stop_after_first = true },
-			},
-		})
+        rust = { "rustfmt", stop_after_first = true },
 
-		-- format shortcut
-		vim.api.nvim_set_keymap("n", "<leader>f", "<cmd>lua require('conform').format()<CR>", {
-			noremap = true,
-			silent = true,
-			desc = "Format with Conform",
-		})
-	end,
+        sql = { "pg_format", stop_after_first = true },
+      },
+    })
+
+    -- format shortcut
+    vim.api.nvim_set_keymap("n", "<leader>f", "<cmd>lua require('conform').format()<CR>", {
+      noremap = true,
+      silent = true,
+      desc = "Format with Conform",
+    })
+  end,
 }
