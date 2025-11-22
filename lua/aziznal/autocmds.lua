071239310478dcbe2571abb9b4d48ab747bc2ca0
aziznal <aziznal.dev@gmail.com>
2025-11-22 10:02:10 +0300

    add autocmd for syntax highlighting all .env files including ones w. suffixes

--- lua/aziznal/autocmds.lua
+++ lua/aziznal/autocmds.lua
@@ -9,7 +9,9 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 
 -- [[ auto resize splits when the terminal's window is resized ]]
 vim.api.nvim_create_autocmd("VimResized", {
-	command = "wincmd =",
+  command = "wincmd =",
+})
+
 -- [[ no auto continue comments on new line ]]
 vim.api.nvim_create_autocmd("FileType", {
   group = vim.api.nvim_create_augroup("no_auto_comment", {}),
@@ -17,6 +19,14 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.formatoptions:remove({ "c", "r", "o" })
   end,
 })
+
+-- [[ syntax highlighting for dotenv files, including ones w. suffixes ]]
+vim.api.nvim_create_autocmd("BufRead", {
+  group = vim.api.nvim_create_augroup("dotenv_ft", { clear = true }),
+  pattern = { ".env", ".env.*" },
+  callback = function()
+    vim.bo.filetype = "dosini"
+  end,
 })
 
 -- [[ remember folds ]]
