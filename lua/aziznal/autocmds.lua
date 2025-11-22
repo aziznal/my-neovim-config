56bd0ed52590e907ff41a531e494aa4fa4d1094c
aziznal <aziznal.dev@gmail.com>
2025-11-22 09:58:20 +0300

    add autocmd to equalize window sizes when terminal is resized

--- lua/aziznal/autocmds.lua
+++ lua/aziznal/autocmds.lua
@@ -1,10 +1,15 @@
 -- [[ highlight yanked group ]]
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- [[ auto resize splits when the terminal's window is resized ]]
+vim.api.nvim_create_autocmd("VimResized", {
+	command = "wincmd =",
 })
 
 -- [[ remember folds ]]
@@ -14,14 +19,14 @@ vim.api.nvim_clear_autocmds({ group = "remember_folds" })
 
 -- save folds when leaving buffers, unless it's a 'help' filetype
 vim.api.nvim_create_autocmd("BufWinLeave", {
-	group = "remember_folds",
-	pattern = "*.*",
-	command = 'if &ft !=# "help" | mkview | endif',
+  group = "remember_folds",
+  pattern = "*.*",
+  command = 'if &ft !=# "help" | mkview | endif',
 })
 
 -- load folds when entering buffers, unless it's a 'help' filetype
 vim.api.nvim_create_autocmd("BufWinEnter", {
-	group = "remember_folds",
-	pattern = "*.*",
-	command = 'if &ft !=# "help" | silent! loadview | endif',
+  group = "remember_folds",
+  pattern = "*.*",
+  command = 'if &ft !=# "help" | silent! loadview | endif',
 })
