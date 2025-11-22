f071cc99cb57ef7e618ca547454ddc73df977996
aziznal <aziznal.dev@gmail.com>
2025-11-22 10:01:50 +0300

    add autocmd to disable auto-continue comments

--- lua/aziznal/autocmds.lua
+++ lua/aziznal/autocmds.lua
@@ -10,6 +10,13 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 -- [[ auto resize splits when the terminal's window is resized ]]
 vim.api.nvim_create_autocmd("VimResized", {
 	command = "wincmd =",
+-- [[ no auto continue comments on new line ]]
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("no_auto_comment", {}),
+  callback = function()
+    vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+  end,
+})
 })
 
 -- [[ remember folds ]]
