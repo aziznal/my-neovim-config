101296f36fd76a7e58ea936104acbad38195242a
aziznal <aziznal.dev@gmail.com>
2025-12-27 11:15:57 +0300

    disable typescript-tools in favor of tsgo
    - tsgo LSP installed with mason

--- lua/aziznal/plugins/typescript-tools.lua
+++ lua/aziznal/plugins/typescript-tools.lua
@@ -1,12 +1,14 @@
-return {
-	"pmizio/typescript-tools.nvim",
-	requires = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-	config = function()
-		require("typescript-tools").setup({})
+-- return {
+-- 	"pmizio/typescript-tools.nvim",
+-- 	requires = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+-- 	config = function()
+-- 		require("typescript-tools").setup({})
+--
+-- 		vim.cmd(":cnoreabbrev rmu TSToolsRemoveUnusedImports")
+-- 		vim.cmd(":cnoreabbrev Rmu TSToolsRemoveUnusedImports")
+-- 		vim.cmd(":cnoreabbrev amu TSToolsAddMissingImports")
+-- 		vim.cmd(":cnoreabbrev Amu TSToolsAddMissingImports")
+-- 	end,
+-- }
 
-		vim.cmd(":cnoreabbrev rmu TSToolsRemoveUnusedImports")
-		vim.cmd(":cnoreabbrev Rmu TSToolsRemoveUnusedImports")
-		vim.cmd(":cnoreabbrev amu TSToolsAddMissingImports")
-		vim.cmd(":cnoreabbrev Amu TSToolsAddMissingImports")
-	end,
-}
+return {}
