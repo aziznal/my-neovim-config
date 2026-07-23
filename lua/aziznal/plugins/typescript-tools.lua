a6f3e8170f77b57a37793a2f93921149f8f8ddf1
aziznal <aziznal.dev@gmail.com>
2026-07-23 08:58:39 +0200

    disable typescript tools

--- lua/aziznal/plugins/typescript-tools.lua
+++ lua/aziznal/plugins/typescript-tools.lua
@@ -1,12 +1,12 @@
 return {
-	"pmizio/typescript-tools.nvim",
-	requires = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-	config = function()
-		require("typescript-tools").setup({})
-
-		vim.cmd(":cnoreabbrev rmu TSToolsRemoveUnusedImports")
-		vim.cmd(":cnoreabbrev Rmu TSToolsRemoveUnusedImports")
-		vim.cmd(":cnoreabbrev amu TSToolsAddMissingImports")
-		vim.cmd(":cnoreabbrev Amu TSToolsAddMissingImports")
-	end,
+	-- "pmizio/typescript-tools.nvim",
+	-- requires = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+	-- config = function()
+	-- 	require("typescript-tools").setup({})
+	--
+	-- 	vim.cmd(":cnoreabbrev rmu TSToolsRemoveUnusedImports")
+	-- 	vim.cmd(":cnoreabbrev Rmu TSToolsRemoveUnusedImports")
+	-- 	vim.cmd(":cnoreabbrev amu TSToolsAddMissingImports")
+	-- 	vim.cmd(":cnoreabbrev Amu TSToolsAddMissingImports")
+	-- end,
 }
